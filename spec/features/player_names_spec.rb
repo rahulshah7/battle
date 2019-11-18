f4ed4efe1b3a6e4e8d939d8d82f5d77ce2aa11d5
Rahul Shah <rahulshah7@outlook.com>
2019-11-18 19:09:50 +0000

    Change play_names_form html class to html id

--- spec/features/player_names_spec.rb
+++ spec/features/player_names_spec.rb
@@ -3,7 +3,7 @@
 feature 'Enter and display player names' do
   scenario 'Each player enters their name' do
     visit '/'
-    within(:css, '.player_names_form') do
+    within(:css, '#player_names_form') do
       fill_in :player1, with: 'Rahul'
       fill_in :player2, with: 'Gabby'
       click_button 'Play!'
