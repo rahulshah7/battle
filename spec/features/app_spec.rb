cd0673479e0eb2c6164bae0e7cd9738e28ea7ee2
Rahul Shah <rahulshah7@outlook.com>
2019-11-18 16:52:51 +0000

    Delete testing infrastructure setup test file.

--- spec/features/app_spec.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-feature 'Test infrastructure setup' do
-  scenario 'Navigate to index page' do
-    visit '/'
-    expect(page).to have_content 'Testing infrastructure working!'
-  end
-end
