acebf1c14c4f8fd84ce07b15975129cde18690a7
Rahul Shah <rahulshah7@outlook.com>
2019-11-19 17:39:30 +0000

    Add player names display

--- app.rb
+++ app.rb
@@ -6,4 +6,10 @@ class Battle < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  post '/names' do
+    @player1 = params['player1']
+    @player2 = params['player2']
+    erb :play
+  end
 end
--- spec/features/player_names_spec.rb
+++ spec/features/player_names_spec.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 feature 'Enter and display player names' do
-  scenario 'Each player enters their name' do
+  scenario 'Each player inputs their names and then submits the form.' do
     visit '/'
     within(:css, '#player_names_form') do
       fill_in :player1, with: 'Rahul'
       fill_in :player2, with: 'Gabby'
       click_button 'Play!'
     end
+    expect(page).to have_content 'Rahul vs. Gabby'
   end
 end
